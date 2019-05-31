e17e40c4df97f7f11dcea759b8a5f9301bebd205
kahles <st-git@kah2.de>
2019-05-31 21:01:05 +0200

    use interpreter title for calendar event name

--- src/main/java/de/kah2/mondtag/calendar/CalendarEvent.java
+++ src/main/java/de/kah2/mondtag/calendar/CalendarEvent.java
@@ -3,6 +3,7 @@ package de.kah2.mondtag.calendar;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.CalendarContract;
+import android.util.Log;
 
 import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
@@ -12,7 +13,9 @@ import org.threeten.bp.ZonedDateTime;
 
 import de.kah2.libZodiac.Day;
 import de.kah2.mondtag.Mondtag;
+import de.kah2.mondtag.MondtagActivity;
 import de.kah2.mondtag.R;
+import de.kah2.mondtag.datamanagement.DataManager;
 
 /**
  * This class is used to build an {@link Intent} to create a calendar event and fill it with data of
@@ -20,16 +23,32 @@ import de.kah2.mondtag.R;
  */
 class CalendarEvent {
 
+    private final static String TAG = CalendarEvent.class.getSimpleName();
+    private final static int DEFAULT_TITLE = R.string.event_default_name;
+
     private final Context context;
     private final long dateInMs;
     private final static int EVENT_DURATION_IN_MS = 86399000;
     private final String description;
+    private final int titleId;
 
     /** Instantiates the object and prepares the needed data. */
     CalendarEvent( final Context context, final Day day ) {
         this.context = context;
         this.dateInMs = this.getDateInMillis( day.getDate() );
+        this.titleId = this.getTitleId();
         this.description = this.createDescription( day );
+    }
+
+    private int getTitleId() {
+        final MappedInterpreter interpreter =
+                ((Mondtag) this.context).getDataManager().getSelectedInterpreter();
+
+        if (interpreter == null) {
+            return DEFAULT_TITLE;
+        } else {
+            return interpreter.getId();
+        }
     }
 
     /** Generates an {@link android.content.Intent} to start a calendar app and add the event. */
@@ -43,7 +62,7 @@ class CalendarEvent {
                         this.dateInMs + EVENT_DURATION_IN_MS)
                 .putExtra( CalendarContract.EXTRA_EVENT_ALL_DAY, true )
                 .putExtra( CalendarContract.Events.TITLE,
-                        context.getString(R.string.event_default_name) )
+                        context.getString(this.titleId) )
                 .putExtra( CalendarContract.Events.DESCRIPTION, this.description )
                 .putExtra( CalendarContract.Events.AVAILABILITY,
                         CalendarContract.Events.AVAILABILITY_FREE )
